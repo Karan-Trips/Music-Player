a3898ec56d04f0af4990cda53dc253580f7521c7
KARAN SHANKAR TRIPATHI <karanboss652@gmail.com>
2025-02-05 15:55:46 +0530

    readme file

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,32 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:audio_service_web/audio_service_web.dart';
+import 'package:audio_session/audio_session_web.dart';
+import 'package:camera_web/camera_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:just_audio_web/just_audio_web.dart';
+import 'package:on_audio_query_web/on_audio_query_web.dart';
+import 'package:permission_handler_html/permission_handler_html.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AudioServiceWeb.registerWith(registrar);
+  AudioSessionWeb.registerWith(registrar);
+  CameraPlugin.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  JustAudioPlugin.registerWith(registrar);
+  OnAudioQueryPlugin.registerWith(registrar);
+  WebPermissionHandler.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
